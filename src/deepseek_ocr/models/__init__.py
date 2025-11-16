36698f71d92c47f1ddd2874dbb3cf439da84b46b
Salim Khazem Macbook-Pro <salimkhazem97@gmail.com>
2025-11-16 17:40:12 +0100

    Adding models

---
+++ src/deepseek_ocr/models/__init__.py
@@ -0,0 +1,12 @@
+"""OCR model implementations."""
+
+from deepseek_ocr.models.deepseek import DeepSeekOCRModel
+from deepseek_ocr.models.smoldocling import SmolDoclingModel
+from deepseek_ocr.models.granite import GraniteDoclingModel
+
+__all__ = [
+    "DeepSeekOCRModel",
+    "SmolDoclingModel",
+    "GraniteDoclingModel",
+]
+
