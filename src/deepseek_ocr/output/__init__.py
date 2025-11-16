fc0a32d2a3ace88fb57cde9a1a88e3eaebfb4572
Salim Khazem Macbook-Pro <salimkhazem97@gmail.com>
2025-11-16 17:43:13 +0100

    Adding Smoldocling

---
+++ src/deepseek_ocr/output/__init__.py
@@ -0,0 +1,18 @@
+"""Output formatters for OCR results."""
+
+from deepseek_ocr.output.formatters import (
+    format_as_html,
+    format_as_json,
+    format_as_markdown,
+    format_as_text,
+    convert_format,
+)
+
+__all__ = [
+    "format_as_markdown",
+    "format_as_html",
+    "format_as_json",
+    "format_as_text",
+    "convert_format",
+]
+
